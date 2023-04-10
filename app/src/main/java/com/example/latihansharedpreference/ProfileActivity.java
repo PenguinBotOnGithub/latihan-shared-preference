4b0b2c57dbec4adecbb55aae5f37ab60c3ea135e
PenguinBotOnGithub <penguinbotonline@protonmail.com>
2023-04-10 09:30:45 +0700

    Upload progress

--- app/src/main/java/com/example/latihansharedpreference/ProfileActivity.java
+++ app/src/main/java/com/example/latihansharedpreference/ProfileActivity.java
@@ -1,12 +1,19 @@
 package com.example.latihansharedpreference;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+
 import com.example.latihansharedpreference.databinding.ActivityProfileBinding;
 
 public class ProfileActivity extends AppCompatActivity {
     private ActivityProfileBinding binding;
+
+    Preferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +21,19 @@ public class ProfileActivity extends AppCompatActivity {
         binding = ActivityProfileBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        preferences = new Preferences(this);
+
+        Button logoutBtn = binding.logout;
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                preferences.setSessionLogin(false);
+
+                Intent intent = new Intent(ProfileActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
